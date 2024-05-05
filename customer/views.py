10f82195c278b7756305df7c785aab5818695131
PSTLTK <g.o.e88uu@gmail.com>
2024-05-05 14:21:57 +0630

    bot

--- customer/views.py
+++ customer/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from myadmin.models import *
+import telepot
 
 # Create your views here.
 
 def HomePage(request):
     herosection = HeroSectionModel.objects.all().order_by('-time')[:1]
+    token = '6779452535:AAFyEG35dthKQJpZz9kOj2RTg46QwbXoukg'    
+    receiver_id = 1842560374
+    bot = telepot.Bot(token)
+    bot.sendMessage(receiver_id, f'New Donate with')
+
     return render(request,'home.html',{"herosection":herosection})
+
+
